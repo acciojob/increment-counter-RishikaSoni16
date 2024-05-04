6d4d35e887102d4374216ddcf5f2aa3bd50e3228
RishikaSoni16 <135811075+RishikaSoni16@users.noreply.github.com>
2024-05-04 23:41:57 +0600

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let para = document.querySelector("#counter");
 let btn = document.querySelector("#incrementBtn");
-let num=1;
+let num=0;
 btn.addEventListener("click",() =>{
 	if(num<=3){
+		num++;
 		para.textContent=num;
 	    alert(num);
-	    num++;}
+	    }
 	});
